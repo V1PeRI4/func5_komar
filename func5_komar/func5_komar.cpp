302ec0635e1c4eb9c52e93e973f8135fecc3b183
V1PeRI4 <V1PeRI4@DESKTOP-C59PJAE>
2021-11-22 22:11:33 +0300

    Добавьте файлы проекта.

---
+++ func5_komar/func5_komar.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+float celsToFaring(float);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	float temp;
+	cout << "Введите градусы в цельсиях: ";
+	cin >> temp;
+	float tempFin = celsToFaring(temp);
+	cout << temp << " градусов Цельсия - это " << tempFin << " градусов по Фарингейту";
+	return 0;
+}
+
+float  celsToFaring(float sts)
+{
+	sts = sts * 1.8 + 32;
+	return sts;
+}
